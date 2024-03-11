828a5459aad56eda2c02d18ceaa4f2623855cd63
Jason <66013853+ahmola@users.noreply.github.com>
2024-03-12 00:55:13 +0900

    react need to be fixed

--- movies-backend/src/main/java/com/example/movies/movie/MovieController.java
+++ movies-backend/src/main/java/com/example/movies/movie/MovieController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin("http://localhost:3000")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/movie")
 @RestController
